966047296474f24898b5b1c309ec718152a65c35
Pavan Sudarshan <itspanzi@gmail.com>
2011-11-03 23:35:28 +0530

    Added the ability to specify working directory to the CommandExecutor.

--- src/com/thoughtworks/studios/javaexec/CommandExecutor.java
+++ src/com/thoughtworks/studios/javaexec/CommandExecutor.java
@@ -2,106 +2,113 @@
 
 package com.thoughtworks.studios.javaexec;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.util.List;
 
 public class CommandExecutor {
 
-  private List<String> cmd;
-  private String standardErrorText;
-  private int returnCode;
-  private Long timeout;
+    private List<String> cmd;
+    private String standardErrorText;
+    private int returnCode;
+    private File workingDir;
+    private Long timeout;
 
-  public CommandExecutor(List<String> cmd) {
-    this(cmd, null);
-  }
-
-  public CommandExecutor(List<String> cmd, Long timeout) {
-    this.cmd = cmd;
-    this.timeout = timeout;
-  }
-
-  public void run(OutputStream outputStream) {
-    try {
-      Process process = new ProcessBuilder(cmd).start();
-      PipeRunnable pipeRunnable = new StreamPipeRunnable(process.getInputStream(), outputStream);
-      Pipe pipe = new Pipe(pipeRunnable);
-      captureOutput(process, pipe);
-    } catch (IOException ioEx) {
-      throw new CommandExecutorException("Command execution failed unexpectedly!", ioEx);
+    public CommandExecutor(List<String> cmd) {
+        this(cmd, null);
     }
-  }
 
-
-  public void run(LineHandler out) {
-    try {
-      Process process = new ProcessBuilder(cmd).start();
-      PipeRunnable pipeRunnable = new LinePipeRunnable(process.getInputStream(), out);
-      Pipe pipe = new Pipe(pipeRunnable);
-      captureOutput(process, pipe);
-    } catch (IOException ioEx) {
-      throw new CommandExecutorException("Command execution failed unexpectedly!", ioEx);
+    public CommandExecutor(List<String> cmd, File workingDir, Long timeout) {
+        this.cmd = cmd;
+        this.workingDir = workingDir;
+        this.timeout = timeout;
     }
-  }
 
-  public String run() {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    run(out);
-    try {
-      return out.toString("UTF-8");
-    } catch (UnsupportedEncodingException ex) {
-      return out.toString();
+    public CommandExecutor(List<String> cmd, File workingDirectory) {
+        this(cmd, workingDirectory, null);
     }
-  }
 
-  public String standardErrorText() {
-    return standardErrorText;
-  }
-
-  public int returnCode() {
-    return returnCode;
-  }
-
-  public boolean isError() {
-    return returnCode != 0;
-  }
-
-  private void captureOutput(Process process, Pipe pipe) {
-    try {
-      ByteArrayOutputStream errorOutput = new ByteArrayOutputStream();
-      StreamPipeRunnable errorPipeRunnable = new StreamPipeRunnable(process.getErrorStream(), errorOutput);
-      Pipe errorPipe = new Pipe(errorPipeRunnable);
-
-      ProcessWatcher watcher = null;
-      if (timeout != null) {
-        watcher = new ProcessWatcher(process, timeout);
-        watcher.start();
-      }
-
-      pipe.start();
-      errorPipe.start();
-
-      pipe.join();
-      errorPipe.join();
-      returnCode = process.waitFor();
-
-      if (watcher != null) {
-        if (watcher.timedOut()) {
-          throw new CommandExecutorException("Command (" + cmd + ") timed out (" + timeout + " msecs)!");
-        } else {
-          watcher.cancel();
+    public void run(OutputStream outputStream) {
+        try {
+            Process process = execute();
+            PipeRunnable pipeRunnable = new StreamPipeRunnable(process.getInputStream(), outputStream);
+            Pipe pipe = new Pipe(pipeRunnable);
+            captureOutput(process, pipe);
+        } catch (IOException ioEx) {
+            throw new CommandExecutorException("Command execution failed unexpectedly!", ioEx);
         }
-      }
-
-      process.destroy();
-
-      standardErrorText = errorOutput.toString();
-
-    } catch (InterruptedException intEx) {
-      throw new CommandExecutorException("Command execution failed unexpectedly!", intEx);
     }
-  }
+
+
+    public void run(LineHandler out) {
+        try {
+            Process process = execute();
+            PipeRunnable pipeRunnable = new LinePipeRunnable(process.getInputStream(), out);
+            Pipe pipe = new Pipe(pipeRunnable);
+            captureOutput(process, pipe);
+        } catch (IOException ioEx) {
+            throw new CommandExecutorException("Command execution failed unexpectedly!", ioEx);
+        }
+    }
+
+    private Process execute() throws IOException {
+        return new ProcessBuilder(cmd).directory(workingDir).start();
+    }
+
+    public String run() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        run(out);
+        try {
+            return out.toString("UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            return out.toString();
+        }
+    }
+
+    public String standardErrorText() {
+        return standardErrorText;
+    }
+
+    public int returnCode() {
+        return returnCode;
+    }
+
+    public boolean isError() {
+        return returnCode != 0;
+    }
+
+    private void captureOutput(Process process, Pipe pipe) {
+        try {
+            ByteArrayOutputStream errorOutput = new ByteArrayOutputStream();
+            StreamPipeRunnable errorPipeRunnable = new StreamPipeRunnable(process.getErrorStream(), errorOutput);
+            Pipe errorPipe = new Pipe(errorPipeRunnable);
+
+            ProcessWatcher watcher = null;
+            if (timeout != null) {
+                watcher = new ProcessWatcher(process, timeout);
+                watcher.start();
+            }
+
+            pipe.start();
+            errorPipe.start();
+
+            pipe.join();
+            errorPipe.join();
+            returnCode = process.waitFor();
+
+            if (watcher != null) {
+                if (watcher.timedOut()) {
+                    throw new CommandExecutorException("Command (" + cmd + ") timed out (" + timeout + " msecs)!");
+                } else {
+                    watcher.cancel();
+                }
+            }
+
+            process.destroy();
+
+            standardErrorText = errorOutput.toString();
+
+        } catch (InterruptedException intEx) {
+            throw new CommandExecutorException("Command execution failed unexpectedly!", intEx);
+        }
+    }
 }
--- test/com/thoughtworks/studios/javaexec/CommandExecutorTest.java
+++ test/com/thoughtworks/studios/javaexec/CommandExecutorTest.java
@@ -2,96 +2,117 @@
 
 package com.thoughtworks.studios.javaexec;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class CommandExecutorTest {
 
-  @Test
-  public void captureOutputWhenStreamingToOutputStream() {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foobar"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    exec.run(outputStream);
-    assertThat(outputStream.toString(), equalTo("foobar"));
-  }
+    private File subFolder;
 
-  @Test
-  public void captureErrorWhenStreamingToOutputStream() {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    String[] cmd = TestProgram.cmdFor(new String[]{"error", "something went wrong", "173"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    exec.run(outputStream);
-
-    assertThat(exec.isError(), equalTo(true));
-    assertThat(exec.standardErrorText(), equalTo("something went wrong"));
-    assertThat(exec.returnCode(), equalTo(173));
-  }
-
-  @Test
-  public void timeoutWhenStreamingtoOutputStream() {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    String[] cmd = TestProgram.cmdFor(new String[]{"sleep", "5000"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd), 100L);
-    try {
-      exec.run(outputStream);
-      fail("should have timed out!");
-    } catch (CommandExecutorException ex) {
-      assertThat(ex.getMessage().contains("timed out"), equalTo(true));
-      assertThat(exec.returnCode(), not(equalTo(0)));
+    @After
+    public void tearDown() throws Exception {
+        if (subFolder != null && subFolder.exists() && !subFolder.delete()) {
+            System.err.println("Failed to delete folder: " + subFolder);
+        }
     }
-  }
 
-  @Test
-  public void captureOutputWhenStreamingToLineHandler() {
-    TestLineHandler lineHandler = new TestLineHandler();
-    String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foo\nbar\n"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    exec.run(lineHandler);
-    String[] expectedLines = new String[]{"foo", "bar"};
-    lineHandler.assertLinesEqual(Arrays.asList(expectedLines));
-  }
+    @Test
+    public void captureOutputWhenStreamingToOutputStream() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foobar"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        exec.run(outputStream);
+        assertThat(outputStream.toString(), equalTo("foobar"));
+    }
 
-  @Test
-  public void captureOutputWithoutStreaming() {
-    String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foobar"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    assertThat(exec.run(), equalTo("foobar"));
-  }
+    @Test
+    public void captureErrorWhenStreamingToOutputStream() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        String[] cmd = TestProgram.cmdFor(new String[]{"error", "something went wrong", "173"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        exec.run(outputStream);
 
-  @Test
-  public void captureChineseOutputWhenStreamingToOutputStream() throws Exception {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    exec.run(outputStream);
-    assertThat(outputStream.toString("UTF-8"), equalTo("这是中文"));
-  }
+        assertThat(exec.isError(), equalTo(true));
+        assertThat(exec.standardErrorText(), equalTo("something went wrong"));
+        assertThat(exec.returnCode(), equalTo(173));
+    }
 
-  @Test
-  public void captureChineseOutputWhenStreamingToLineHandler() {
-    TestLineHandler lineHandler = new TestLineHandler();
-    String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    exec.run(lineHandler);
-    String[] expectedLines = new String[]{"这是中文"};
-    lineHandler.assertLinesEqual(Arrays.asList(expectedLines));
-  }
+    @Test
+    public void timeoutWhenStreamingtoOutputStream() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        String[] cmd = TestProgram.cmdFor(new String[]{"sleep", "5000"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd), null, 100L);
+        try {
+            exec.run(outputStream);
+            fail("should have timed out!");
+        } catch (CommandExecutorException ex) {
+            assertThat(ex.getMessage().contains("timed out"), equalTo(true));
+            assertThat(exec.returnCode(), not(equalTo(0)));
+        }
+    }
 
-  @Test
-  public void captureChineseOutputWhenNotStreaming() throws Exception {
-    String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
-    CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
-    assertThat(exec.run(), equalTo("这是中文"));
-  }
+    @Test
+    public void captureOutputWhenStreamingToLineHandler() {
+        TestLineHandler lineHandler = new TestLineHandler();
+        String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foo\nbar\n"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        exec.run(lineHandler);
+        String[] expectedLines = new String[]{"foo", "bar"};
+        lineHandler.assertLinesEqual(Arrays.asList(expectedLines));
+    }
+
+    @Test
+    public void captureOutputWithoutStreaming() {
+        String[] cmd = TestProgram.cmdFor(new String[]{"echo", "foobar"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        assertThat(exec.run(), equalTo("foobar"));
+    }
+
+    @Test
+    public void captureOutputWhenRunningInADifferentDir() throws IOException {
+        subFolder = new File(new File(".").getCanonicalPath(), "subFolder");
+        boolean mkdir = subFolder.mkdir();
+        assertTrue("Failed to create sub-directory: " + mkdir, mkdir);
+        CommandExecutor exec = new CommandExecutor(Arrays.asList("pwd"), subFolder);
+        String out = exec.run();
+        assertThat(new File(out.trim()).getAbsolutePath(), equalTo(subFolder.getAbsolutePath()));
+    }
+
+    @Test
+    public void captureChineseOutputWhenStreamingToOutputStream() throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        exec.run(outputStream);
+        assertThat(outputStream.toString("UTF-8"), equalTo("这是中文"));
+    }
+
+    @Test
+    public void captureChineseOutputWhenStreamingToLineHandler() {
+        TestLineHandler lineHandler = new TestLineHandler();
+        String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        exec.run(lineHandler);
+        String[] expectedLines = new String[]{"这是中文"};
+        lineHandler.assertLinesEqual(Arrays.asList(expectedLines));
+    }
+
+    @Test
+    public void captureChineseOutputWhenNotStreaming() throws Exception {
+        String[] cmd = TestProgram.cmdFor(new String[]{"chinese"});
+        CommandExecutor exec = new CommandExecutor(Arrays.asList(cmd));
+        assertThat(exec.run(), equalTo("这是中文"));
+    }
 
 //  @Test
 //  public void README_example_one() throws Exception {
